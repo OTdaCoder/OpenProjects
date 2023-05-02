0bec3664256eaa19f3c76065445631a04669a37e
OTdaCoder <106548757+OTdaCoder@users.noreply.github.com>
2023-05-02 21:36:46 +0200

    Add files via upload

---
+++ yt2text/youtube-transcriber/components/output.tsx
@@ -0,0 +1,30 @@
+import React , { useEffect, useRef } from 'react';
+import { styled } from '@stitches/react';
+import { Box } from './box';
+
+type Props ={
+    children: string | undefined
+}
+
+export const Output : React.FC<Props> = ({children} ) => {
+    const refBox = useRef <HTMLDivElement>(null)
+
+    useEffect(() => {
+        const elBox = refBox.current
+        if (elBox) {
+            elBox.scrollTop = elBox.scrollHeight
+        }
+    }, [children])
+
+    return (
+        <Box css={{ flex: 1, overflow: 'auto' }} className='output-container'>
+            <pre>{children}</pre>
+        </Box>
+    )
+}
+
+export const Pre = styled('pre', {
+    margin: '1em',
+    fontSize: '1.2em',
+    whiteSpace: 'pre-wrap'
+})
