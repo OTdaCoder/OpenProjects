0e39e7515ca3da122de1e7d178f359e4776c8c74
OTdaCoder <106548757+OTdaCoder@users.noreply.github.com>
2023-05-16 14:21:48 +0200

    Add files via upload

--- yt2text/youtube-transcriber/pages/api/transcript.ts
+++ yt2text/youtube-transcriber/pages/api/transcript.ts
@@ -2,7 +2,6 @@ import { request } from 'http'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { spawn } from 'child_process'
 import path from 'path'
-import Script from 'next/script'
 import { transferChildProcessOutput } from '../../utils/shell'
 
 export default function GET(
@@ -15,7 +14,8 @@ export default function GET(
     return
   }
   console.log('Video ID:', video_id)
-  const cmd = spawn(path.join(process.cwd(), 'scripts/download-audio.sh'), [
+  const cmd = spawn('python3', [
+    path.join(process.cwd(), 'scripts/transcribe.py'),
     video_id || ''
   ])
   transferChildProcessOutput(cmd, response)
